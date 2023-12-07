828a41e9d95167fefbb9bd52275ae9e45e9a7a88
Egor Gavrilenko <bambloo769@gmail.com>
2023-12-07 21:09:41 +0300

    fix: layout header

--- src/app/core/components/layout/layout.component.ts
+++ src/app/core/components/layout/layout.component.ts
@@ -1,49 +1,14 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MessageService, MenuItem } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
-import { MenuModule } from 'primeng/menu';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [CommonModule, ButtonModule, MenuModule],
-  providers: [MessageService],
+  imports: [CommonModule, RouterModule, ButtonModule],
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LayoutComponent implements OnInit {
-  items: MenuItem[] | undefined;
-
-  constructor(private messageService: MessageService) {}
-
-  ngOnInit() {
-    this.items = [
-      {
-        items: [
-          {
-            label: 'Profile',
-            icon: 'pi pi-user',
-            routerLink: '/profile',
-          },
-          {
-            label: 'Logout',
-            icon: 'pi pi-sign-out',
-            command: () => {
-              this.delete();
-            },
-          },
-        ],
-      },
-    ];
-  }
-
-  update() {
-    this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Data Updated' });
-  }
-
-  delete() {
-    this.messageService.add({ severity: 'warn', summary: 'Delete', detail: 'Data Deleted' });
-  }
-}
+export class LayoutComponent {}
