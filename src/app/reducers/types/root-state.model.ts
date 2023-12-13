c7fbbc9a50e0a183dab7aab56587062b24b83f6c
Egor Gavrilenko <bambloo769@gmail.com>
2023-12-13 23:05:16 +0300

    chore: add new reducers to root reducer

--- src/app/reducers/types/root-state.model.ts
+++ src/app/reducers/types/root-state.model.ts
@@ -1,7 +1,10 @@
-import { AuthState } from '../../auth/types';
-import { UserInfoState } from '../../user/types';
+import { AuthState } from '@auth/types';
+import { GroupsState, UsersState } from '@messenger/types';
+import { UserInfoState } from '@user/types';
 
 export interface RootState {
   auth: AuthState;
   user: UserInfoState;
+  groups: GroupsState;
+  users: UsersState;
 }
